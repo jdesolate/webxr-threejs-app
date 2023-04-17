68f58b206fc86baf110de07045c9bb3f20a136e5
Merv Tampus <79685609+jdesolate@users.noreply.github.com>
2023-04-17 21:49:35 +0800

    [Misc] Tutorialspoint Cube

--- client/script.js
+++ client/script.js
@@ -1,62 +1,73 @@
-import * as THREE from "three";
+import * as dat from "dat.gui";
+// Adding folders to distinguish between variables
 
-// Set up the camera and scene
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
-);
+// controls
+const gui = new dat.GUI();
+// sizes
+let width = window.innerWidth;
+let height = window.innerHeight;
+// scene
 const scene = new THREE.Scene();
-
-// Create a plane for the video feed
-const planeGeometry = new THREE.PlaneGeometry(16, 10);
-const planeMaterial = new THREE.MeshBasicMaterial({
+scene.background = new THREE.Color(0x262626);
+// camera
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100);
+camera.position.set(0, 0, 10);
+const camFolder = gui.addFolder("Camera");
+camFolder.add(camera.position, "z").min(10).max(60).step(10);
+// cube
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({
   color: 0xffffff,
-  side: THREE.DoubleSide,
+  wireframe: true,
 });
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.position.set(0, 0, -10);
-scene.add(plane);
-
-// Set up the renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-const aspect = window.innerWidth / window.innerHeight;
-camera.aspect = aspect;
-camera.updateProjectionMatrix();
-
-document.body.appendChild(renderer.domElement);
-
-const video = document.createElement("video");
-video.autoplay = true;
-const texture = new THREE.VideoTexture(video);
-planeMaterial.map = texture;
-const material = new THREE.MeshBasicMaterial({ map: texture });
-const geometry = new THREE.PlaneGeometry(16, 10);
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-  video.srcObject = stream; // replace with socket video stream
+const cubeColor = {
+  color: 0xffffff,
+};
+const materialFolder = gui.addFolder("Material");
+materialFolder.add(material, "wireframe");
+materialFolder.addColor(cubeColor, "color").onChange(() => {
+  // callback
+  material.color.set(cubeColor.color);
 });
-
-// Create a 3D object to display on top of the video feed
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.position.set(0, 0, -5);
+materialFolder.open();
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
-// Render the scene
-function render() {
-  requestAnimationFrame(render);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+const cubeFolder = gui.addFolder("Cube");
+// for position
+const posFolder = cubeFolder.addFolder("position");
+posFolder.add(cube.position, "x", 0, 5, 0.1);
+posFolder.add(cube.position, "y", 0, 5, 0.1);
+posFolder.add(cube.position, "z", 0, 5, 0.1);
+posFolder.open();
+// for scale
+const scaleFolder = cubeFolder.addFolder("Scale");
+scaleFolder.add(cube.scale, "x", 0, 5, 0.1).name("Width");
+scaleFolder.add(cube.scale, "y", 0, 5, 0.1).name("Height");
+scaleFolder.add(cube.scale, "z", 0, 5, 0.1).name("Depth");
+scaleFolder.open();
+cubeFolder.open();
+// responsiveness
+window.addEventListener("resize", () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.render(scene, camera);
-
-  texture.needsUpdate = true;
+});
+// renderer
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(width, height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// animation
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.005;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
-
-render();
+// rendering the scene
+const container = document.querySelector("#threejs-container");
+container.append(renderer.domElement);
+renderer.render(scene, camera);
+animate();
